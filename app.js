a4339c2cc931b9ea7624fb4ef3873eecb72e08cc
Aleksandr <viking26@mail.ru>
2022-06-21 10:28:21 +0300

    rr module

--- app.js
+++ app.js
@@ -6,12 +6,12 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-if (process.env.NODE_ENV === 'production') {
-  console.log('imhelelele');
-  app.get('/*', (req, res) => {
-    req.sendFile(path.resolve(__dirname, 'build', 'index.html'));
-  });
-}
+// if (process.env.NODE_ENV === 'production') {
+//   console.log('imhelelele');
+//   app.get('/*', (req, res) => {
+//     req.sendFile(path.resolve(__dirname, 'build', 'index.html'));
+//   });
+// }
 app.use(express.static('build'));
 // const indexRouter = require('./routes/indexRouter');
 
